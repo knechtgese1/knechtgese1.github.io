39f50f87d36ff9a796e11b325da2451b19f6c750
Eric Knechtges <eric.knechtges@array.com>
2023-01-17 09:22:35 -0500

    Add tooltip

--- hup-chart/src/routes/_components/chart.svelte
+++ hup-chart/src/routes/_components/chart.svelte
@@ -20,6 +20,11 @@
   let coords = dataSet.map(() => [0, 0]);
   let hypotenuses = dataSet.map(() => 0);
   let angles = dataSet.map(() => 0);
+  let hovered = dataSet.map(() => false);
+
+  const getTooltip = (i: number) => {
+    return [data.data[i].dateString + '\u00A0' + data.data[i].timeString, data.data[i].value + (data.units || '')];
+  }
 
   $: if (width || height) {
     coords = dataSet.map(point => [(width * (point[0] - minXvalue) / (maxXvalue - minXvalue)), (height * (point[1] - minYvalue) / (maxYvalue - minYvalue))]);
@@ -37,7 +42,11 @@
     {#each coords as [x, y], i}
       <li style="--y: {y}px; --x: {x}px;">
         <div class="line-segment" style="--hypotenuse: {hypotenuses[i]}; --angle: {angles[i]}" />
-        <div class="data-point" data-x={dataSet[i][0]} data-y={dataSet[i][1]} />
+        <div class="data-point" data-x={dataSet[i][0]} data-y={dataSet[i][1]} on:mouseenter={() => hovered[i] = true} on:mouseleave={() => hovered[i] = false}/>
+        <div class="tooltip" class:hovered={hovered[i]}>
+          {getTooltip(i)[0]}<br>
+          {getTooltip(i)[1]}
+        </div>
       </li>
     {/each}
   </ul>
@@ -71,8 +80,36 @@
     left: calc(var(--x) - 3px);
   }
 
+  .tooltip {
+    font-family:Arial, Helvetica, sans-serif;
+    font-size: 1rem;
+    font-weight: 900;
+    z-index: 1;
+    position: relative;
+    bottom: 1.25rem;
+    color: darkred;
+    background: lightgrey;
+    padding: 3px;
+    opacity: 0.75;
+    position: absolute;
+    bottom: calc(var(--y) + 5px);
+    left: calc(var(--x) - 1px);
+    display: none;
+  }
+
+  .tooltip.hovered {
+    display: block;
+  }
+
+  .data-point:hover {
+    height: 10px;
+    width: 10px;
+    bottom: calc(var(--y) - 4px);
+    left: calc(var(--x) - 5px);
+  }
+
   .line-segment {
-    background-color: red;
+    background-color: #888888;
     bottom: var(--y);
     height: 1px;
     left: var(--x);
